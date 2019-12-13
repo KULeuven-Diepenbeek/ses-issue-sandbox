7cc1774d171d43cb124de27c84109d74c818996e
wgroeneveld <wouter.groeneveld@gmail.com>
2019-12-13 10:04:11 +0100

    sand castles - yay...

---
+++ src/main/java/be/kuleuven/SandboxMain.java
@@ -0,0 +1,21 @@
+package be.kuleuven;
+
+import be.kuleuven.sandbox.BucketOfWater;
+import be.kuleuven.sandbox.SandCastle;
+
+import java.util.Arrays;
+
+public class SandboxMain {
+
+    public static void main(String[] args) {
+        var list = Arrays.asList(
+                new SandCastle(),
+                new BucketOfWater()
+        );
+
+        System.out.println("A day at the sea in the sand: \n");
+        for (var playItem : list) {
+            playItem.play();
+        }
+    }
+}

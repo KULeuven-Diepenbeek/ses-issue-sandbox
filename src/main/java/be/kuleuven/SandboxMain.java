7594811f9d16c8c731e3e26f2c85e6141f62186f
LuigiGuerriero1999 <60965016+LuigiGuerriero1999@users.noreply.github.com>
2020-02-20 16:38:35 +0100

    candyboy

--- src/main/java/be/kuleuven/SandboxMain.java
+++ src/main/java/be/kuleuven/SandboxMain.java
@@ -1,7 +1,10 @@
 package be.kuleuven;
 
 import be.kuleuven.sandbox.BucketOfWater;
+import be.kuleuven.sandbox.Candy;
 import be.kuleuven.sandbox.SandCastle;
+
+
 
 import java.util.Arrays;
 
@@ -10,7 +13,8 @@ public class SandboxMain {
     public static void main(String[] args) {
         var list = Arrays.asList(
                 new SandCastle(),
-                new BucketOfWater()
+                new BucketOfWater(),
+                new Candy()
         );
 
         System.out.println("A day at the sea in the sand: \n");
---
+++ src/main/java/be/kuleuven/sandbox/Candy.java
@@ -0,0 +1,8 @@
+package be.kuleuven.sandbox;
+
+public class Candy implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Super yummy candy niffo!");
+    }
+}

5b547ba7649b19cfc142802e358ac783a22f046c
Arne Duyver <61016144+ArneDuyver@users.noreply.github.com>
2020-02-20 16:00:51 +0100

    Alle issues resolved.

---
+++ src/main/java/be/kuleuven/sandbox/FishingNet.java
@@ -0,0 +1,8 @@
+package be.kuleuven.sandbox;
+
+public class FishingNet implements Playable {
+    @Override
+    public void play() {
+        System.out.println("I fish fish fish with my fishing net!");
+    }
+}
---
+++ src/main/java/be/kuleuven/sandbox/Magazines.java
@@ -0,0 +1,8 @@
+package be.kuleuven.sandbox;
+
+public class Magazines implements Playable {
+    @Override
+    public void play() {
+        System.out.println("I got me some Magazines, yeah!");
+    }
+}

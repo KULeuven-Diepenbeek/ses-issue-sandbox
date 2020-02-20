7fca63411c710110a284b75eaa4dd0ea6b1fab34
maartenbaeten509 <maartenbaeten509@users.noreply.github.com>
2020-02-20 15:55:20 +0100

    soukabletsjkkkie

--- src/main/java/be/kuleuven/SandboxMain.java
+++ src/main/java/be/kuleuven/SandboxMain.java
@@ -1,6 +1,7 @@
 package be.kuleuven;
 
 import be.kuleuven.sandbox.BucketOfWater;
+import be.kuleuven.sandbox.JellyFish;
 import be.kuleuven.sandbox.SandCastle;
 
 import java.util.Arrays;
@@ -10,7 +11,8 @@ public class SandboxMain {
     public static void main(String[] args) {
         var list = Arrays.asList(
                 new SandCastle(),
-                new BucketOfWater()
+                new BucketOfWater(),
+                new JellyFish()
         );
 
         System.out.println("A day at the sea in the sand: \n");

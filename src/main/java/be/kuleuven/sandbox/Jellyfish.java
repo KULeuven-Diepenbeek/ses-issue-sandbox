47f8e7278a9658a6274976593f5720b1ed3a1299
nielscapiot <60965164+nielscapiot@users.noreply.github.com>
2020-02-20 16:13:14 +0100

    added jellyfish class

---
+++ src/main/java/be/kuleuven/sandbox/Jellyfish.java
@@ -0,0 +1,11 @@
+package be.kuleuven.sandbox;
+
+public class Jellyfish implements Playable {
+    @Override
+    public void play() {
+        System.out.println("I caught a jellyfish");
+    }
+}
+
+
+

89807e86aef67d2b299207bad577264c2b51e166
S. Arne <Arne_schepers@hotmail.com>
2021-02-17 15:51:33 +0100

    Creating new branch

--- src/main/java/be/kuleuven/SandboxMain.java
+++ src/main/java/be/kuleuven/SandboxMain.java
@@ -12,6 +12,7 @@ public class SandboxMain {
         var list = Arrays.asList(
                 new SandCastle(),
                 new BucketOfWater()
+
         );
 
         System.out.println("A day at the sea in the sand: \n");

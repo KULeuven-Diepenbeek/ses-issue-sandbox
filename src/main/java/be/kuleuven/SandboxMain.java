669bb1b81a320819efb8f377cda022e185cdef71
Vossen98 <57484882+Vossen98@users.noreply.github.com>
2021-02-24 13:27:23 +0100

    First commit

--- src/main/java/be/kuleuven/SandboxMain.java
+++ src/main/java/be/kuleuven/SandboxMain.java
@@ -2,6 +2,7 @@ package be.kuleuven;
 
 import be.kuleuven.sandbox.BucketOfWater;
 import be.kuleuven.sandbox.SandCastle;
+import be.kuleuven.sandbox.Candy;
 
 import java.lang.String;
 import java.util.Arrays;
@@ -11,6 +12,7 @@ public class SandboxMain {
     public static void main(String[] args) {
         var list = Arrays.asList(
                 new SandCastle(),
+                new Candy(),
                 new BucketOfWater()
         );
 
---
+++ src/main/java/be/kuleuven/sandbox/Candy.java
@@ -0,0 +1,8 @@
+package be.kuleuven.sandbox;
+
+public class Candy implements Playable {
+    @Override
+    public void play() {
+        System.out.println("I'm carrying a bag of candy!");
+    }
+}

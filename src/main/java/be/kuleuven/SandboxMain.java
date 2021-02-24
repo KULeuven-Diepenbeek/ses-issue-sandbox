f41bf3cff40eb1d1aa284f3356d51ea28fe705bb
arnepapen <arne.papen@student.uhasselt.be>
2021-02-24 13:24:28 +0100

    Samengevoegd

--- src/main/java/be/kuleuven/SandboxMain.java
+++ src/main/java/be/kuleuven/SandboxMain.java
@@ -1,11 +1,8 @@
 package be.kuleuven;
 
 import be.kuleuven.sandbox.BucketOfWater;
-<<<<<<< HEAD
 import be.kuleuven.sandbox.Icecream;
-=======
 import be.kuleuven.sandbox.JellyFish;
->>>>>>> 8fe0cd7999af5c7c12e39a7f4d18bb5b5c7cbd9b
 import be.kuleuven.sandbox.SandCastle;
 
 import java.lang.String;
@@ -17,11 +14,8 @@ public class SandboxMain {
         var playList = Arrays.asList(
                 new SandCastle(),
                 new BucketOfWater(),
-<<<<<<< HEAD
-                new Icecream()
-=======
+                new Icecream(),
                 new JellyFish()
->>>>>>> 8fe0cd7999af5c7c12e39a7f4d18bb5b5c7cbd9b
         );
 
         System.out.println("Yay!");

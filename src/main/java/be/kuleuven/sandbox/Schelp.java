e599b61c634ae6f2037e333c51b1cc2229cceb6e
Maxet1000 <maxemile.meylaerts@gmail.com>
2023-02-15 14:30:17 +0100

    added schelp to main

--- src/main/java/be/kuleuven/sandbox/Schelp.java
+++ src/main/java/be/kuleuven/sandbox/Schelp.java
@@ -1,2 +1,10 @@
-package be.kuleuven.sandbox;public class Schelp {
+package be.kuleuven.sandbox;
+
+public class Schelp implements Playable{
+
+
+    @Override
+    public void play() {
+        System.out.println("Yeey, schelpen rapen op het strand!");
+    }
 }

a7e5b8e54bf148c5012e5ae87488045b4f8b02e1
Dries04 <dries.ruttens@student.uhasselt.be>
2023-02-16 11:44:47 +0100

    extra commit

--- src/main/java/be/kuleuven/sandbox/SandCastle.java
+++ src/main/java/be/kuleuven/sandbox/SandCastle.java
@@ -5,5 +5,6 @@ public class SandCastle implements Playable {
     @Override
     public void play() {
         System.out.println("Look at my sand castle! Wow!");
+        System.out.println("dit is een test");
     }
 }
